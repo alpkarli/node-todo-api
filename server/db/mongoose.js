0b415a7b92b68c26cad5f0349f5020b48cc08a08
Alperen <surqurer@gmail.com>
2017-06-27 13:05:35 +0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-var uri = mongodb://surqurer:qw3r4sdf.@ds139242.mlab.com:39242/todo-app_alperen;
+var uri = "mongodb://surqurer:qw3r4sdf.@ds139242.mlab.com:39242/todo-app_alperen";
 mongoose.connect(uri || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
