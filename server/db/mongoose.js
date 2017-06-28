aced607d27bbb29654eb0d17710a5abba8c7c2a0
Alperen <surqurer@gmail.com>
2017-06-28 03:09:00 +0300

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 // var uri = "mongodb://surqurer:qw3r4sdf.@ds139242.mlab.com:39242/todo-app_alperen";
 // mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://surqurer:qw3r4sdf.@ds139242.mlab.com:39242/todo-app_alperen');
 
